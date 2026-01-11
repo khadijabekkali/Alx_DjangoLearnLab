04d810eee2d9a56bc394c6e622a47ddcc877d4b4
Khadija Bekkali <khadijabekkali44@gmail.com>
2026-01-11 15:26:25 +0100

    Fix admin.py: import Book, register model, add list_display, list_filter, search_fields

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Book  # <-- this is required
 
-# Register your models here.
+# Customize the Book admin
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')  # columns to show in list view
+    list_filter = ('author', 'publication_year')            # sidebar filters
+    search_fields = ('title', 'author')                     # search box
+
+# Register the Book model with the customized admin
+admin.site.register(Book, BookAdmin)
