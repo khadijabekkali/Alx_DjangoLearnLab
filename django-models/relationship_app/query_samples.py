c526cd74f5a8c715e2fbe5e91f42007d7ea5b4f6
Khadija Bekkali <khadijabekkali44@gmail.com>
2026-01-18 19:37:57 +0100

    file the files

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -1,19 +1,23 @@
+import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_models.settings")
+django.setup()
+
 from relationship_app.models import Author, Book, Library, Librarian
 
+# Example queries:
 
-# 1. Query all books by a specific author
-def get_books_by_author(author_name):
-    author = Author.objects.get(name=author_name)
-    return author.books.all()
+# Query all books by a specific author
+def books_by_author(author_name):
+    return Book.objects.filter(author__name=author_name)
 
-
-# 2. List all books in a library
-def get_books_in_library(library_name):
+# List all books in a library
+def books_in_library(library_name):
     library = Library.objects.get(name=library_name)
     return library.books.all()
 
-
-# 3. Retrieve the librarian for a library
-def get_librarian_for_library(library_name):
+# Retrieve the librarian for a library
+def librarian_of_library(library_name):
     library = Library.objects.get(name=library_name)
     return library.librarian
